5416c724f1c1e7867b91c7367879453009821be0
allie516 <allie516@f0d08c57-3c6c-7442-4b12-595821e7a180>
2012-01-20 04:18:40 +0000

    Initial commit.

---
+++ trunk/src/com/alliecurry/guitarChords/GuitarChordsActivity.java
@@ -0,0 +1,13 @@
+package com.alliecurry.guitarChords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuitarChordsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
