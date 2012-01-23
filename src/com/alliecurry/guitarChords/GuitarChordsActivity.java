c3f24d2adb7ca5011c01fb7dee04eba854a33d0e
allie516 <allie516@f0d08c57-3c6c-7442-4b12-595821e7a180>
2012-01-23 01:26:09 +0000

    * Updated Buttons + Activity for the "Learn" Screen + Layout (base) for the "Learn" Screen

--- src/com/alliecurry/guitarChords/GuitarChordsActivity.java
+++ src/com/alliecurry/guitarChords/GuitarChordsActivity.java
@@ -1,7 +1,10 @@
 package com.alliecurry.guitarChords;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GuitarChordsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,26 @@ public class GuitarChordsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        createListeners();
+    }
+    
+    private void createListeners() {
+    	//Import Buttons
+    	Button b_learn = (Button) findViewById(R.id.button_learn);
+    	Button b_listen = (Button) findViewById(R.id.button_listen);
+    	
+    	//Go to LearnActivity
+    	b_learn.setOnClickListener(new View.OnClickListener(){	
+	    	public void onClick(View v){
+	    		Intent i = new Intent(getApplicationContext(), LearnActivity.class);
+	    		startActivity(i);
+	    	}
+    	});
+    	
+    	//Go to ListenActivity
+    	b_listen.setOnClickListener(new View.OnClickListener(){	
+	    	public void onClick(View v){
+	    	}
+    	});
     }
 }
---
+++ src/com/alliecurry/guitarChords/LearnActivity.java
@@ -0,0 +1,14 @@
+package com.alliecurry.guitarChords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LearnActivity extends Activity {
+
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learn);
+    }
+}
